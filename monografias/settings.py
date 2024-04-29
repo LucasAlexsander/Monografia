932ec1f514aa57e723a7de1fcd07c512e555d6b8
Iago Fernandes <iago.avila@ufvjm.edu.br>
2024-04-29 09:06:04 -0300

    Ajuste no docker compose/arquivo sttings

--- monografias/settings.py
+++ monografias/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
+
 from pathlib import Path
 # from decouple import config
 
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-)n7*=l$d9w)d7=j@5_hpo&ttejs1r(k*ftnz+lp(qj%urv!24x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG",default=0))
+DEBUG = True
 
-ALLOWED_HOSTS =  os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -78,15 +78,16 @@ WSGI_APPLICATION = 'monografias.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'root',  # Nome do banco de dados
+        'USER': 'root',  # Nome do usuário
+        'PASSWORD': 'root',  # Senha do usuário
+        'HOST': 'localhost',  # Host onde o PostgreSQL está em execução (no caso de usar Docker Compose localmente, "localhost" funciona)
+        'PORT': '5434',  # Porta exposta pelo Docker Compose
     }
 }
+
 
 
 # Password validation
