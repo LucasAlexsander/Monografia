88903a62f165f973698c341056871eac6d61b966
Lucas Alexsander <lucas10barbosa_@hotmail.com>
2024-04-30 00:21:23 -0300

    Adiciona Ordenação

--- documentos/views.py
+++ documentos/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Documentos
 from . forms import DocumentosForm
 from django.http import FileResponse, HttpResponseNotFound, JsonResponse
+from django.urls import reverse
 from documentos.functions import handle_uploaded_file
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import os
 
 def view_pdf(request, filename):
@@ -12,9 +14,39 @@ def view_pdf(request, filename):
     else:
         return HttpResponseNotFound('PDF not found')
 
-def listar_documentos(resquest):
+def listar_documentos(request):
+    ordenacao = request.GET.get('ordenacao', 'default')  # Obter parâmetro de ordenação da query string
     documentos = Documentos.objects.all()
-    return render(resquest, 'documentos.html', {'Documentos': documentos})
+    ordenacao_direcao = ''
+
+    if ordenacao == 'titulo':
+        documentos = documentos.order_by('titulo')
+    elif ordenacao == '-titulo':
+        documentos = documentos.order_by('-titulo')
+    elif ordenacao == 'data':
+        documentos = documentos.order_by('dataEntrega')
+    elif ordenacao == '-data':
+        documentos = documentos.order_by('-dataEntrega')
+
+    # Paginação
+    paginator = Paginator(documentos, 8)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    page_numbers = [n for n in range(page_obj.number - 2, page_obj.number + 3) if 0 < n <= page_obj.paginator.num_pages] 
+    
+    # Adicione o parâmetro de ordenação à URL dos links de paginação
+    print("Query string:", request.META['QUERY_STRING'])
+
+    if ordenacao:
+        ordenacao_param = f"&ordenacao={ordenacao}"
+        page_obj.url_suffix = ordenacao_param if '?' in request.META['QUERY_STRING'] else f"?ordenacao={ordenacao}"
+
+
+    # Verificar se o número da página é menor que 1 e, se for, definir como 1
+    if page_obj.number < 1:
+        page_obj.number = 1
+        
+    return render(request, 'documentos.html', {'Documentos': page_obj, 'page_numbers': page_numbers, 'ordenacao': ordenacao})
 
 def adicionar(request):
     form = DocumentosForm(request.POST, request.FILES)
