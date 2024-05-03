13c65cd81d2800410dc335eec09b2b8a55fca318
Iago Fernandes <iago.avila@ufvjm.edu.br>
2024-05-03 08:24:13 -0300

    Ajuste para retorno Json

--- equipe/urls.py
+++ equipe/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.listar_equipe, name='listar_equipe')
+    path('', views.listar_equipe, name='listar_equipe'),
+    path('Json/', views.listar_equipeJson, name='listar_equipeJson'),
+    path('cadastrar/', views.cadastrar_pesquisador, name='cadastrar_pesquisador'),  # Nova rota para cadastrar pesquisador
 ]
--- equipe/views.py
+++ equipe/views.py
@@ -1,10 +1,42 @@
 from django.shortcuts import render
 from .models import Pesquisador
-
-# Create your views here.
-# def listar_equipe(resquest):
-#     return render(resquest, 'equipe.html')
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import JsonResponse 
 
 def listar_equipe(resquest):
     pesquisadores = Pesquisador.objects.all()
     return render(resquest, 'equipe.html', {'pesquisadores': pesquisadores})
+
+def listar_equipeJson(resquest):
+    pesquisadores = Pesquisador.objects.all()
+    pesquisadores_data = list(pesquisadores.values())
+    return JsonResponse(pesquisadores_data, safe=False)
+
+# Serve para esativar proteção CSRF para uma view específica no Django.
+@csrf_exempt
+def cadastrar_pesquisador(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        nome = data.get('nome')
+        nivel = data.get('nivel')
+        lattes = data.get('lattes')
+        linkedin = data.get('linkedin')
+        researchgate = data.get('researchgate')
+        email = data.get('email')
+        cargo = data.get('cargo')
+
+        pesquisador = Pesquisador.objects.create(
+            nome=nome,
+            nivel=nivel,
+            lattes=lattes,
+            linkedin=linkedin,
+            researchgate=researchgate,
+            email=email,
+            cargo=cargo
+        )
+
+        return JsonResponse({'mensagem': 'Pesquisador cadastrado com sucesso!'})
+
+    else:
+        return JsonResponse({'erro': 'Apenas requisições POST são permitidas.'}, status=405)
